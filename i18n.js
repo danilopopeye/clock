631ff9d46a4d7011ac97561e3b2f1aededf4a1ad
Danilo Sousa <danilopopeye@gmail.com>
2010-07-19 02:22:39 -0300

    Make i18n number to text;
    As only has the pt-BR numbers, forced this language for now;

--- i18n.js
+++ i18n.js
@@ -3,8 +3,24 @@
 		getTitle: function(){
 		  return this[ this.user ].title;
 		},
-		getHour: function(h){
-			console.info( this, arguments );
+		number: function(n){
+			if( typeof n !== 'number' || n > 99 ){
+				return '';
+			}
+
+			var self = this[ this.user ];
+
+			if( n < 10 ){
+				return self.number[ n ];
+			} else if ( n % 10 == 0 ){
+				return self.decimal[ n / 10 ];
+			} else if ( n < 20  ){
+				return self.other[ n ];
+			}
+
+			n = n.toString();
+
+			return self.decimal[ n[0] ] + self.decimal[0] + self.number[ n[1] ];
 		},
 		"pt-BR": {
 			title: "Relógio",
@@ -12,8 +28,20 @@
 				h: "hora",
 				m: "minuto",
 				s: "segundo"
+			},
+			number: [ 'zero','um','dois','tres','quatro','cinco','seis','sete','oito','nove' ],
+			decimal: [ ' e ','dez','vinte','trinta','quarenta','cinquenta','sessenta','setenta','oitenta','noventa' ],
+			other: {
+				11: 'onze',
+				12: 'doze',
+				13: 'treze',
+				14: 'quatorze',
+				15: 'quinze',
+				16: 'dezesseis',
+				17: 'dezessete',
+				18: 'dezoito',
+				19: 'dezenove'
 			}
-			
 		},
 		"en-US": {
 			title: "Clock",
@@ -26,5 +54,5 @@
 	};
 
 	// if fail set en-US
-	i18n.user = navigator.userLanguage || window.navigator.language || "en-US";
+	i18n.user = "pt-BR" || navigator.userLanguage || window.navigator.language || "en-US";
 })();
